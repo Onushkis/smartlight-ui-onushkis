be4d51e29f398a8875f444d6e774ea5baef7b2bc
Ona <onushkis@gmail.com>
2022-02-01 14:38:42 +0100

    added changes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="border-2 border-rose-500 w-96  text-3xl font-bold text-regal-pink">HolaMOna</h1>
+    </div>
+  );
+}
+
+export default App;
