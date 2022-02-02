e1d19d0d2099cef959c74b4ce5950533c215fcba
Ona <onushkis@gmail.com>
2022-02-02 17:14:39 +0100

    added changes blue back

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="border-2 border-rose-500 w-96  text-3xl font-bold text-regal-pink">HolaMOna</h1>
+    <div className="App" > 
+     
+     <div className="relative">
+     <div className="bg-regal-bluebacgrounf w-375 h-812 absolute inset-x-0 top-0">
+       <div class="text-[#fff]">Control panel</div>
+       
+    </div>
+
+
+</div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
+
+    backgroundSize: {
+      'auto': 'auto',
+      'cover': 'cover',
+      'contain': 'contain',
+      '50%': '50%',
+      '16': '4rem',
+    },
     extend: {
+      width: {
+        '375': '23rem',
+      },
+      height: {
+        '812': '51rem',
+      },
       colors: {
-        'regal-pink': '#FFADF0',
+        'regal-violet': '#A693EB',
+        'regal-red': '#FF9B9B',
+        'regal-blue': '#93CAEB',
+        'regal-green': '#89DD94',
+        'regal-yellow': '#EBD094',
+        'regal-violetbtn': '#A594EB',
+        'regal-bluebacgrounf': '#0A4DA2',
+        'regal-yellowtext': '#FFA939',
+        'regal-bluetext': '#002D67',
+        'regal-blacktext': '#393939',
+
 
       },
     },
